050df2959abddcfe238d423cfed199cd1c8a7ebc
Bimarsh Sharma <bimarshsharma101@gmail.com>
2020-12-03 15:25:49 -0500

    Inject DAO Implementation to Controller Class and add data from DAO to Model

--- web-customer-tracker/src/com/bimarsh/spring/controller/CustomerController.java
+++ web-customer-tracker/src/com/bimarsh/spring/controller/CustomerController.java
@@ -1,15 +1,32 @@
 package com.bimarsh.spring.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bimarsh.spring.dao.CustomerDAO;
+import com.bimarsh.spring.entities.CustomerEntity;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
 	
+	//Inject DAO into this controller
+	@Autowired
+	private CustomerDAO customerDAO;
+	
+	
 	@RequestMapping("/list")
-	public String listCustomers(Model theModel) {
+	public String listCustomers(Model customerModel) {
+		
+		//get customers from the DAO
+		List<CustomerEntity> customers = customerDAO.getCustomers();
+		
+		//add the customers to the model
+		customerModel.addAttribute("customers", customers); // provide attribute name and value
 		
 		return "list-customer";
 	}
