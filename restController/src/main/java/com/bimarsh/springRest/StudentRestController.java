e58d33338fe9974344f873b73740828a51cb7fd6
Bimarsh Sharma <bimarshsharma101@gmail.com>
2020-12-05 23:20:39 -0500

    Refactored Rest Controller by adding @PostConstruct to load data before bean initalization

--- restController/src/main/java/com/bimarsh/springRest/StudentRestController.java
+++ restController/src/main/java/com/bimarsh/springRest/StudentRestController.java
@@ -3,6 +3,8 @@ package com.bimarsh.springRest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +15,23 @@ import com.bimarsh.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
-	//define endpoint for "/students" - return list of students
-	
-	@GetMapping("/students")
-	public List<Student> getStudent(){
-		
-		List<Student> students = new ArrayList<Student>();
-		
+	private List<Student> students;
+
+	// define @PostConstruct to load the student data... only once!
+	@PostConstruct
+	public void loadData() {
+		students = new ArrayList<Student>();
+
 		students.add(new Student("Bimarsh", "Sharma"));
 		students.add(new Student("Timmy", "Trumpet"));
-		students.add(new Student("Marshall", "Mathers"));		
-		
+		students.add(new Student("Marshall", "Mathers"));
+	}
+
+	// define endpoint for "/students" - return list of students
+
+	@GetMapping("/students")
+	public List<Student> getStudent() {
+		//no need to call because Spring will call @PostConstruct before initializing the bean
 		return students;
 	}
 }
