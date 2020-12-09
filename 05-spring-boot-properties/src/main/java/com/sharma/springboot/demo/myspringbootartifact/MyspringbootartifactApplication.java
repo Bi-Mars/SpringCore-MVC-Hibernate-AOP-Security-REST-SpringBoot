aa62b9ea6aaccea28688c92392475e42d94e70b7
Bimarsh Sharma <bimarshsharma101@gmail.com>
2020-12-08 21:10:09 -0500

    Added and used values from application.properties

---
+++ 05-spring-boot-properties/src/main/java/com/sharma/springboot/demo/myspringbootartifact/MyspringbootartifactApplication.java
@@ -0,0 +1,13 @@
+package com.sharma.springboot.demo.myspringbootartifact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyspringbootartifactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyspringbootartifactApplication.class, args);
+	}
+
+}
