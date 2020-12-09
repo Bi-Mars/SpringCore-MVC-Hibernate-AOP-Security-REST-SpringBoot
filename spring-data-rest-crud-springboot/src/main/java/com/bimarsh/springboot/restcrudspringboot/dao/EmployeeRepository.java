380a440d192f7c1568ccdb4df127a74c8c8b64b4
Bimarsh Sharma <bimarshsharma101@gmail.com>
2020-12-09 10:39:40 -0500

    Give custom endpoint for Repository

--- spring-data-rest-crud-springboot/src/main/java/com/bimarsh/springboot/restcrudspringboot/dao/EmployeeRepository.java
+++ spring-data-rest-crud-springboot/src/main/java/com/bimarsh/springboot/restcrudspringboot/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.bimarsh.springboot.restcrudspringboot.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.bimarsh.springboot.restcrudspringboot.entity.Employee;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
 	//that's it ... no need to write any code
