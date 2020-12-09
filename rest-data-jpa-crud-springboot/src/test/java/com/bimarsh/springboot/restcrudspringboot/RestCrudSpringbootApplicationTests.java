a2ff9812a1d5e820bb42ed5c8002b215a9de946d
Bimarsh Sharma <bimarshsharma101@gmail.com>
2020-12-09 09:43:23 -0500

    Used Spring Data JPA for DAO, reduces code by more than 70%

---
+++ rest-data-jpa-crud-springboot/src/test/java/com/bimarsh/springboot/restcrudspringboot/RestCrudSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bimarsh.springboot.restcrudspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCrudSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
