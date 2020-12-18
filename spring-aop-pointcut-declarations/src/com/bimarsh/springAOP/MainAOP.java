efa709a5d61b94289d3079f5919ba61245822f85
Bimarsh Sharma <bimarshsharma101@gmail.com>
2020-12-18 18:51:41 -0500

    Added @pointcut declaration for multiple use of same pointcut expression

---
+++ spring-aop-pointcut-declarations/src/com/bimarsh/springAOP/MainAOP.java
@@ -0,0 +1,38 @@
+package com.bimarsh.springAOP;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.bimarsh.springAOP.dao.AccountDAO;
+import com.bimarsh.springAOP.dao.MembershipDAO;
+
+public class MainAOP {
+
+	public static void main(String[] args) {
+		
+		//read spring configuration java class
+		AnnotationConfigApplicationContext context =
+				new AnnotationConfigApplicationContext(SpringAOPConfig.class);
+		
+		
+		//get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		
+		//get Membership bean fro spring container
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+		
+		//call the business method
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+		
+		//call the membership business method
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+		
+		
+		//close the context
+		context.close();
+
+	}
+
+}
---
+++ spring-aop-pointcut-declarations/src/com/bimarsh/springAOP/dao/AccountDAO.java
@@ -0,0 +1,18 @@
+package com.bimarsh.springAOP.dao;
+
+import org.springframework.stereotype.Component;
+
+import com.bimarsh.springAOP.Account;
+
+@Component
+public class AccountDAO {
+
+	public void addAccount(Account theAccount, boolean vipFlag) {
+		System.out.println(getClass() + ": Doing my DB work: Adding and Account");
+	}
+	
+	public boolean doWork() {
+		System.out.println(getClass() + ": Doing Stuff");
+		return false;
+	}
+}
